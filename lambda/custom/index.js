fb3e6cff75106e67f63e90b90d605cb8fcff9290
Ruman Saleem <ruman63@gmail.com>
2018-10-13 22:39:15 +0530

    Initial Commit

---
+++ lambda/custom/index.js
@@ -0,0 +1,108 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+const Quotes = require('./quotations');
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Welcome to the Wisdom Words, You can ask me for a quotation!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Welcome to Wisdom Words', speechText)
+      .getResponse();
+  },
+};
+
+const QuotationIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'QuotationIntent';
+  },
+  handle(handlerInput) {
+    const random = Math.ceil(Math.random() * Quotes.length * 10) % Quotes.length;
+    const quote = Quotes[random]; 
+    const speechText = quote.quote + '\n -by ' + quote.author;
+
+    return handlerInput.responseBuilder
+      .speak(quote.quote)
+      .reprompt("You can ask me for another quotation!")
+      .withSimpleCard('Today\'s words of wisdom' , speechText)
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can ask for a quotation!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Wisdom Words', speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Wisdom Words', speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    QuotationIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
---
+++ lambda/custom/quotations.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        quote: "Be curious. Read widely. Try new things. I think a lot of what people call intelligence boils down to curiosity.",
+        author: "Aaron Swartz"
+    },
+    {
+        quote: "We sort of understood abstractly the idea that there are only two kinds of software projects: failures and future legacy horrors.",
+        author: "Peter Weinberger"
+    },
+    {
+        quote: "The amateur software engineer is always in search of magic.",
+        author: "Grady Booch"
+    },
+    {
+        quote: "The only way to learn a new programming language is by writing programs in it.",
+        author: "Dennis Ritchie"
+    }
+];
